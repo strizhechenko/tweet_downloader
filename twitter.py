fe675813f3e0ecb94b788b53065889bd17dbe8d3
Oleg Strizhechenko <oleg@carbonsoft.ru>
2015-09-20 19:48:01 +0500

    remove twitting code

--- twitter.py
+++ twitter.py
@@ -20,24 +20,9 @@ class TwiBot(threading.Thread):
         auth.set_access_token(conf['access_token'], conf[
                               'access_token_secret'])
         return tweepy.API(auth)
-
-    def tweet(self, tweet):
-        if len(tweet) <= 140 and len(tweet) > 0:
-            try:
-                self.api.update_status(tweet)
-                return True
-            except tweepy.TweepError:
-                print 'Duplicate'
-        return False
+        
 
     def __init__(self, config=os.getenv('HOME') + '/etc/twibot.ini'):
             threading.Thread.__init__(self)
     	self.config = config
     	self.api = self.conf2api()
-
-
-if __name__ == '__main__':
-    if os.path.isfile(sys.argv[1]):
-        TwiBot(sys.argv[1]).tweet(" ".join(sys.argv[2:]))
-    else:
-        TwiBot().tweet(" ".join(sys.argv[1:]))
