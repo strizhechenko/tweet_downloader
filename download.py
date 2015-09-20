22f18868b7024dd7ccb5ea67f051436cc57a896a
Oleg Strizhechenko <oleg@carbonsoft.ru>
2015-09-20 19:46:43 +0500

    Create download.py

---
+++ download.py
@@ -0,0 +1,31 @@
+from twitter import TwiBot
+import json, os
+
+HOME = os.getenv('HOME')
+
+cache = HOME + '/tweet_cache.json'
+config = HOME + '/etc/twibot.ini'
+
+
+def tweets_save(tweets):
+    open(cache, 'w').write(json.dumps(tweets, indent=4))
+
+
+def tweets_fetch_all():
+    me = TwiBot(config).api.me()
+    all_tweets = list()
+    max_id = None
+    for page in range(0, 16):  # 16 * 200 = 3200, twitter limit
+        tweets, max_id = download(me, max_id)
+        all_tweets.extend(tweets)
+    return all_tweets
+
+
+def tweets_fetch(me, max_id):
+    tweets = me.timeline(count=200, max_id=max_id)
+    tweets_to_text = lambda tweet: tweet.text
+    return map(tweets_to_text, tweets), tweets.max_id
+
+
+if __name__ == '__main__':
+    tweets_save(tweets_fetch_all())
